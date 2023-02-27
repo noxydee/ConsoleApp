1a76bb7c1225fa22de6d220e74f3d47f8be14d73
noxydee <tomeksok28@gmail.com>
2023-02-27 01:00:54 +0100

    Added consts class

---
+++ ConsoleApp/Utils/ProjectConsts.cs
@@ -0,0 +1,13 @@
+﻿namespace ConsoleApp.Utils
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public static class ProjectConsts
+    {
+        public static string Database = "DATABASE";
+    }
+}
