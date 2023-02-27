cd1300fbe55a540bc4adef614be76d1aeca06eff
noxydee <tomeksok28@gmail.com>
2023-02-27 01:00:45 +0100

    Added extension methods

---
+++ ConsoleApp/Utils/ArrayUtils.cs
@@ -0,0 +1,12 @@
+﻿namespace ConsoleApp.Utils
+{
+    using System;
+
+    public static class ArrayUtils
+    {
+        public static T GetValueOrDefault<T>(this T[] array, int index)
+        {
+            return index < array.Length ? array[index] : default(T);
+        }
+    }
+}
---
+++ ConsoleApp/Utils/StringUtils.cs
@@ -0,0 +1,28 @@
+﻿namespace ConsoleApp.Utils
+{
+    using System.Xml.Linq;
+    using System;
+
+    public static class StringUtils
+    {
+        public static string TrimAndReplaceNewLine(this string source)
+        {
+            if (source != null)
+            {
+                return source.Trim().Replace(" ", "").Replace(Environment.NewLine, "");
+            }
+
+            return source;
+        }
+
+        public static bool EqualsIgnoreCase(this string source, string target)
+        {
+            if (source != null && target != null)
+            {
+                return source.ToUpper() == target.ToUpper();
+            }
+
+            return false;
+        }
+    }
+}
