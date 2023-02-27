4e1cb52a8b0f243e296f1fde2ebf0793c8c74e04
noxydee <tomeksok28@gmail.com>
2023-02-27 01:01:25 +0100

    Added data models

---
+++ ConsoleApp/Models/ImportedObject.cs
@@ -0,0 +1,45 @@
+﻿namespace ConsoleApp.Models
+{
+    using ConsoleApp.Utils;
+
+    using System;
+   
+    internal class ImportedObject : ImportedBaseObject
+    {
+        public string Schema { get; set; }
+        public string ParentName { get; set; }
+        public string ParentType { get; set; }
+        public string DataType { get; set; }
+        public string IsNullable { get; set; }
+        public double NumberOfChildren { get; set; }
+
+        public ImportedObject(string[] values)
+        {
+            Type = values.GetValueOrDefault(0);
+            Name = values.GetValueOrDefault(1);
+            Schema = values.GetValueOrDefault(2);
+            ParentName = values.GetValueOrDefault(3);
+            ParentType = values.GetValueOrDefault(4);
+            DataType = values.GetValueOrDefault(5);
+            IsNullable = values.GetValueOrDefault(6);
+
+            ClearAndCorrectData();
+        }
+
+        public void ClearAndCorrectData()
+        {
+            Type = Type.TrimAndReplaceNewLine();
+            Name = Name.TrimAndReplaceNewLine();
+            Schema = Schema.TrimAndReplaceNewLine();
+            ParentName = ParentName.TrimAndReplaceNewLine();
+            ParentType = ParentType.TrimAndReplaceNewLine();
+
+            Type = Type != null ? Type.ToUpper() : Type;
+        }
+
+        public bool IsChildren(ImportedObject potentialChildren)
+        {
+            return Type == potentialChildren.ParentType && Name == potentialChildren.ParentName;
+        }
+    }
+}
---
+++ ConsoleApp/Models/ImportedObjectBase.cs
@@ -0,0 +1,8 @@
+﻿namespace ConsoleApp.Models
+{
+    internal abstract class ImportedBaseObject
+    {
+        public string Name { get; set; }
+        public string Type { get; set; }
+    }
+}
