28edd86859f324df73db8f07961988d7e9446e9b
noxydee <tomeksok28@gmail.com>
2023-02-27 01:01:15 +0100

    Cleaned Main method

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,18 +1,11 @@
 ﻿namespace ConsoleApp
 {
-    using System;
-    using System.Collections.Generic;
-    using System.IO;
-    using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
-
-    internal class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            var reader = new DataReader();
-            reader.ImportAndPrintData("dataa.csv");
+            DataReader reader = new DataReader();
+            reader.ImportAndPrintData("data.csv");
         }
     }
 }
